167fc1e23ed5eb3a875a752ab5837d66a1644baa
Jesse Fernandez <jfernandez@coderunner.studio>
2023-02-14 12:00:20 -0600

    Updated Migrations to Include 'Production' Option

--- backend/db/migrations/20230214052202-create-groupimage.js
+++ backend/db/migrations/20230214052202-create-groupimage.js
@@ -1,39 +1,48 @@
 "use strict";
-/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Groupimages", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      "Groupimages",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        url: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        groupid: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        preview: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      url: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      groupid: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      preview: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-    });
+      options
+    );
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Groupimages");
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = "Groupimages";
+    return queryInterface.dropTable(options);
   },
 };
--- backend/db/migrations/20230214063700-create-membership.js
+++ backend/db/migrations/20230214063700-create-membership.js
@@ -1,39 +1,48 @@
 "use strict";
-/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Memberships", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      "Memberships",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        status: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        userid: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        groupid: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      status: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      userid: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      groupid: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-    });
+      options
+    );
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Memberships");
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = "Memberships";
+    return queryInterface.dropTable(options);
   },
 };
